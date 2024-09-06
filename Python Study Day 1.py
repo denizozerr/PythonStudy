af52416a4ad271f7746548249d0cb6b16b512443
denizozerr <denizozer58@gmail.com>
2024-09-06 15:53:01 +0300

    Day_1

---
+++ Python Study Day 1.py
@@ -0,0 +1,25 @@
+print("Hello World")
+#%%
+
+print(" Hello world! \n Hello world! \n Hello world!")
+#%%
+
+print("Merhaba "+input("Adınızı yazın") + "!")
+#%%
+
+print(len(input("What is your name?")))
+#%%
+
+ad = input("Adınızı girin: ")
+kelime=len(ad)
+print(kelime)
+#%%
+
+name = "Deniz"
+length = len(name)
+print(length)
+#%%
+
+glass1 = input("1. bardaktaki içeceği yazın: ")
+glass2 = input("2. bardaktaki içeceği yazın: ")
+print(glass1 + " ve " + glass2)
